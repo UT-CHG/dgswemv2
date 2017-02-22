a734e8cf208aebd288073669b4e6dbb1bf0b2e38
kazbekkm <kazbek@ices.utexas.edu>
2017-02-22 00:34:19 -0600

    Add files via upload

---
+++ source/Integration rules/integration_rules_line.h
@@ -0,0 +1,4 @@
+int gausslegendre_degree(int p);
+int gausslegendre_number_gp(int p);
+void gausslegendre_rule(int number_gp, double z[], double w[]);
+void gausslegendre_rule_test(int p, int number_gp, double z[], double w[]);
