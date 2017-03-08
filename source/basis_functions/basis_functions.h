f29888a22512dda8a8fab7d84a88a89e94bc3809
kazbekkm <kazbek@ices.utexas.edu>
2017-03-07 23:24:20 -0600

    test

--- source/basis_functions/basis_functions.h
+++ source/basis_functions/basis_functions.h
@@ -4,5 +4,3 @@ void jacobi_polynomial_derivative(int n, int a, int b, int number_gp, double x[]
 void dubiner_phi(int p, int q, int number_gp, double n1[], double n2[], double phi_pq[]);
 void dubiner_dphi(int p, int q, int number_gp, double n1[], double n2[], double dphi_dz1[], double dphi_dz2[]);
 void dubiner_test(int p, int number_gp, double** phi_area, double* w);
-
-TESTING
