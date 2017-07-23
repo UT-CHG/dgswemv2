14a73f2ccc1179056e1bc4461d6aed72d03046a8
kazbekkm <kazbek@ices.utexas.edu>
2017-07-23 08:02:32 -0500

    refix

--- source/problem/SWE/swe_ic_src_functions.hpp
+++ source/problem/SWE/swe_ic_src_functions.hpp
@@ -112,30 +112,30 @@ namespace SWE {
 		double x = pt[GlobalCoord::x];
 		double y = pt[GlobalCoord::y];
 		
-		return w*zo*cos((t + tau)*w)*cos(w*(y - y1))*(1. / (w*(-x1 + x2)))*(1. / cos(w*(-y1 + y2)))*
-			sin(w*(x - x1)) - 2 * SWE::Global::g*Ho*w*zo*cos((t + tau)*w)*cos(w*(x - x1))*
-			(1. / (w*(-x1 + x2)))*(1. / cos(w*(-y1 + y2)))*sin(w*(y - y1)) -
+		return w*zo*cos((t + tau)*w)*cos(w*(x - x1))*(1. / cos(w*(-x1 + x2)))*(1. / cos(w*(-y1 + y2)))*
+			sin(w*(y - y1)) - 2 * SWE::Global::g*Ho*w*zo*cos((t + tau)*w)*cos(w*(x - x1))*
+			(1. / cos(w*(-x1 + x2)))*(1. / cos(w*(-y1 + y2)))*sin(w*(y - y1)) -
 			4.*SWE::Global::g*w*pow(zo, 2)*pow(cos((t + tau)*w), 2)*pow(cos(w*(x - x1)), 2)*
-			cos(w*(y - y1))*pow((1. / (w*(-x1 + x2))), 2)*pow((1. / cos(w*(-y1 + y2))), 2)*
+			cos(w*(y - y1))*pow((1. / cos(w*(-x1 + x2))), 2)*pow((1. / cos(w*(-y1 + y2))), 2)*
 			sin(w*(y - y1)) + (3 * w*pow(zo, 2)*pow(cos(w*(x - x1)), 2)*cos(w*(y - y1))*
-				pow((1. / (w*(-x1 + x2))), 2)*pow((1. / cos(w*(-y1 + y2))), 2)*
+				pow((1. / cos(w*(-x1 + x2))), 2)*pow((1. / cos(w*(-y1 + y2))), 2)*
 				pow(sin((t + tau)*w), 2)*sin(w*(y - y1))) /
-				(Ho + 2 * zo*cos((t + tau)*w)*cos(w*(x - x1))*cos(w*(y - y1))*(1. / (w*(-x1 + x2)))*
+				(Ho + 2 * zo*cos((t + tau)*w)*cos(w*(x - x1))*cos(w*(y - y1))*(1. / cos(w*(-x1 + x2)))*
 			(1. / cos(w*(-y1 + y2)))) + (2 * w*pow(zo, 3)*cos((t + tau)*w)*cos(w*(x - x1))*
-				pow(cos(w*(y - y1)), 2)*pow((1. / (w*(-x1 + x2))), 3)*
+				pow(cos(w*(y - y1)), 2)*pow((1. / cos(w*(-x1 + x2))), 3)*
 				pow((1. / cos(w*(-y1 + y2))), 3)*pow(sin((t + tau)*w), 2)*pow(sin(w*(x - x1)), 2)*
 				sin(w*(y - y1))) /
 			pow(Ho + 2 * zo*cos((t + tau)*w)*cos(w*(x - x1))*cos(w*(y - y1))*
-			(1. / (w*(-x1 + x2)))*(1. / cos(w*(-y1 + y2))), 2) -
-				(w*pow(zo, 2)*cos(w*(y - y1))*pow((1. / (w*(-x1 + x2))), 2)*
+			(1. / cos(w*(-x1 + x2)))*(1. / cos(w*(-y1 + y2))), 2) -
+				(w*pow(zo, 2)*cos(w*(y - y1))*pow((1. / cos(w*(-x1 + x2))), 2)*
 					pow((1. / cos(w*(-y1 + y2))), 2)*pow(sin((t + tau)*w), 2)*pow(sin(w*(x - x1)), 2)*
 					sin(w*(y - y1))) /
-					(Ho + 2 * zo*cos((t + tau)*w)*cos(w*(x - x1))*cos(w*(y - y1))*(1. / (w*(-x1 + x2)))*
+					(Ho + 2 * zo*cos((t + tau)*w)*cos(w*(x - x1))*cos(w*(y - y1))*(1. / cos(w*(-x1 + x2)))*
 			(1. / cos(w*(-y1 + y2)))) + (2 * w*pow(zo, 3)*cos((t + tau)*w)*
-				pow(cos(w*(x - x1)), 3)*pow((1. / (w*(-x1 + x2))), 3)*
+				pow(cos(w*(x - x1)), 3)*pow((1. / cos(w*(-x1 + x2))), 3)*
 				pow((1. / cos(w*(-y1 + y2))), 3)*pow(sin((t + tau)*w), 2)*pow(sin(w*(y - y1)), 3))
 			/ pow(Ho + 2 * zo*cos((t + tau)*w)*cos(w*(x - x1))*cos(w*(y - y1))*
-			(1. / (w*(-x1 + x2)))*(1. / cos(w*(-y1 + y2))), 2);
+			(1. / cos(w*(-x1 + x2)))*(1. / cos(w*(-y1 + y2))), 2);
 	}
 }
 
