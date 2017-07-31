0c83094c61b58460d46b13e322654f401b2fa4c8
Maximilian Bremer <max@ices.utexas.edu>
2017-07-31 13:06:58 -0500

    X

--- scripts/circleci/build.sh
+++ scripts/circleci/build.sh
@@ -7,7 +7,8 @@ cd $HOME
 git clone https://github.com/jbeder/yaml-cpp.git
 
 cd $HOME/dgswemv2/scripts/
-build/build-yaml-cpp.sh -c circleci/test.config.txt
+#Use an implicit newline in echo to simulate hitting the enter key
+echo | build/build-yaml-cpp.sh -c circleci/test.config.txt
 
 cd $HOME/dgswemv2
 mkdir build
