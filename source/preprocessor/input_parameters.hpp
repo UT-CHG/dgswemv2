e6a2c580312b8897bf491aab68e5b67a094a02cb
Maximilian Bremer <max@ices.utexas.edu>
2017-08-02 13:16:03 -0500

    Adding missing input_parameters files (`.cpp`, `.hpp`)

---
+++ source/preprocessor/input_parameters.hpp
@@ -0,0 +1,30 @@
+#ifndef INPUT_HPP
+#define INPUT_HPP
+
+#include "mesh_metadata.hpp"
+
+struct RKInput
+{
+  uint nstages;
+  uint order;
+};
+
+struct InputParameters
+{
+  InputParameters(const char* input_string);
+
+  std::string mesh_file_name;
+  MeshMetaData mesh_data;
+
+  //right now we only support SSPRK timestepping
+  RKInput rk;
+
+  //time parameters
+  double dt;
+  //double T_start;
+  double T_end;
+
+  uint polynomial_order;
+
+};
+#endif
