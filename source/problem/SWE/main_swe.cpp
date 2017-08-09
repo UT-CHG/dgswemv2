569fc47f4ff953cc07b99091c2e30d68b2a56ba1
kazbekkm <kazbek@ices.utexas.edu>
2017-08-08 23:20:33 -0500

    try

--- source/problem/SWE/main_swe.cpp
+++ source/problem/SWE/main_swe.cpp
@@ -12,12 +12,31 @@
 #include <hpx/hpx.hpp>
 
 int main(int argc, const char* argv[]) {
-    boost::program_options::options_description desc_commandline("Usage: " HPX_APPLICATION_STRING " [options]");
-    
-    desc_commandline.add_options()
+namespace po = boost::program_options;
+
+// Declare the supported options.
+po::options_description desc("Allowed options");
+desc.add_options()
     ("help", "produce help message")
-    ("compression", po::value<int>(), "set compression level");
-    
+    ("compression", po::value<int>(), "set compression level")
+;
+
+po::variables_map vm;
+po::store(po::parse_command_line(ac, av, desc), vm);
+po::notify(vm);    
+
+if (vm.count("help")) {
+    cout << desc << "\n";
+    return 1;
+}
+
+if (vm.count("compression")) {
+    cout << "Compression level was set to " 
+ << vm["compression"].as<int>() << ".\n";
+} else {
+    cout << "Compression level was not set.\n";
+}
+
     if (argc != 2) {
         std::cerr << "Usage\n"
                   << "    /path/to/DG_HYPER_SWE input_file\n";
