53b9319fd0ec1680a4f15d2fa40d8fa3c5da9a32
Maximilian Bremer <max@ices.utexas.edu>
2017-08-09 11:25:10 -0500

    Attempting to set-up circle-ci for hpx branch

---
+++ scripts/circleci/set-up-dependencies.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cmake --version && g++ --version
+
+#clone yaml-cpp
+cd $HOME
+git clone https://github.com/jbeder/yaml-cpp.git
+
+cd $HOME/dgswemv2/scripts/
+#Use an implicit newline in echo to simulate hitting the enter key
+echo | build/build-yaml-cpp.sh -c circleci/test.config.txt
+
+#hpx
+cd $HOME
+git clone https://github.com/STEllAR-GROUP/hpx.git
+echo | build/build-hpx.sh -c circleci/test.config.txt
