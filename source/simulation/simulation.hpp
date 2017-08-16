a7b69f8d51550ee3e26173ce618013a667163d48
kazbekkm <kazbek@ices.utexas.edu>
2017-08-15 22:12:21 -0500

    simulation clas

---
+++ source/simulation/simulation.hpp
@@ -0,0 +1,86 @@
+#ifndef SIMULATION_HPP
+#define SIMULATION_HPP
+
+template <typename ProblemType> 
+class Simulation {
+private:
+    typename ProblemType::mesh_type mesh;
+    Stepper stepper;
+
+public:
+    Simulation(std::string);
+
+    void RunSimulation(double);
+};
+
+template <typename ProblemType>
+Simulation<ProblemType>::Simulation(std::string input_string) {
+    const InputParameters input(input_string.c_str());
+
+    printf("Starting program with p=%d for %s mesh\n\n", input.polynomial_order, input.mesh_file_name.c_str());
+            
+    this->stepper = Stepper(input.rk.nstages, input.rk.order, input.dt);
+
+    this->mesh = typename ProblemType::mesh_type(input.polynomial_order, input.mesh_data._mesh_name);
+
+    initialize_mesh<SWE::Problem>(mesh, input.mesh_data);
+}
+
+template <typename ProblemType>
+void Simulation<ProblemType>::RunSimulation(double time_end) {
+    // we write these gross looking wrapper functions to append the stepper in a way that allows us to keep the
+    // the nice std::for_each notation without having to define stepper within each element
+    auto volume_kernel = [&stepper](auto& elt) { ProblemType::volume_kernel(stepper, elt); };
+
+    auto source_kernel = [&stepper](auto& elt) { ProblemType::source_kernel(stepper, elt); };
+
+    auto interface_kernel = [&stepper](auto& intface) { ProblemType::interface_kernel(stepper, intface); };
+
+    auto boundary_kernel = [&stepper](auto& bound) { ProblemType::boundary_kernel(stepper, bound); };
+
+    auto update_kernel = [&stepper](auto& elt) { ProblemType::update_kernel(stepper, elt); };
+
+    auto swap_states_kernel = [&stepper](auto& elt) { ProblemType::swap_states_kernel(stepper, elt); };
+
+    auto scrutinize_solution_kernel = [&stepper](auto& elt) { ProblemType::scrutinize_solution_kernel(stepper, elt); };
+
+    uint nsteps = (uint)std::ceil(time_end / this->stepper.get_dt());
+    uint n_stages = this->stepper.get_num_stages();
+
+    auto resize_data_container = [n_stages](auto& elt) { elt.data.resize(n_stages); };
+
+    this->mesh.CallForEachElement(resize_data_container);
+
+    ProblemType::write_VTK_data_kernel(this->stepper, this->mesh);
+    ProblemType::write_modal_data_kernel(this->stepper, this->mesh);
+
+    hpx::future<void> future = hpx::make_ready_future();
+
+    for (uint step = 1; step <= nsteps; ++step) {
+        for (uint stage = 0; stage < this->stepper.get_num_stages(); ++stage) {
+            this->mesh.CallForEachElement(volume_kernel);
+
+            this->mesh.CallForEachElement(source_kernel);
+
+            this->mesh.CallForEachInterface(interface_kernel);
+
+            this->mesh.CallForEachBoundary(boundary_kernel);
+
+            this->mesh.CallForEachElement(update_kernel);
+
+            this->mesh.CallForEachElement(scrutinize_solution_kernel);
+
+            this->stepper++;
+        }
+
+        this->mesh.CallForEachElement(swap_states_kernel);
+
+        if (step % 360 == 0) {
+            std::cout << "Step: " << step << "\n";
+            ProblemType::write_VTK_data_kernel(this->stepper, this->mesh);
+            ProblemType::write_modal_data_kernel(this->stepper, this->mesh);
+        }
+    }
+}
+
+#endif
