affdfbd6c994c975de5b31d1fea2d7ff8b752f53
Kazbek Kazhyken <kazbek@kepler.ices.utexas.edu>
2017-08-15 19:55:25 -0500

    mesh wrapper

---
+++ source/hpx/hpx_mesh.hpp
@@ -0,0 +1,16 @@
+#ifndef HPX_MESH_HPP
+#define HPX_MESH_HPP
+
+template <typename ProblemType>
+class hpx_mesh : public hpx::components::simple_component_base<hpx_mesh<ProblemType>> {
+  public:
+    typename ProblemType::mesh_type mesh;
+
+    hpx_mesh() : mesh(1, "empty") {};  // default constructor to register hpx component
+
+    hpx_mesh(uint p, const MeshMetaData& mesh_data) : mesh(p, mesh_data._mesh_name) {
+        initialize_mesh<ProblemType>(this->mesh, mesh_data);
+    };
+};
+
+#endif
