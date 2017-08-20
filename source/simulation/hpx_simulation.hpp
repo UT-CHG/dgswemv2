34b266ed1fa1edcd9c525cbdde8c16d1a05f9b51
Kazbek Kazhyken <kazbek@kepler.ices.utexas.edu>
2017-08-19 19:56:41 -0500

    stepper timestamp

--- source/simulation/hpx_simulation.hpp
+++ source/simulation/hpx_simulation.hpp
@@ -83,7 +83,7 @@ hpx::future<void> HPXSimulation<ProblemType>::Run(double time_end) {
     for (uint step = 1; step <= nsteps; step++) {
         for (uint stage = 0; stage < this->stepper.get_num_stages(); stage++) {
             future = future.then([this, &volume_kernel, &source_kernel, &interface_kernel](auto&&) {
-                this->Send(this->stepper.get_timestamp(), this->stepper.get_t_at_curr_stage());
+                this->Send(this->stepper.get_timestamp(), this->stepper.get_timestamp());
 
                 this->mesh.CallForEachElement(volume_kernel);
 
