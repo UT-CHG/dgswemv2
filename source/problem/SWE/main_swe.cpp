d8a98beb03a81fad2d21738051732c4ede701d10
kazbekkm <kazbek@ices.utexas.edu>
2017-08-27 18:59:16 -0500

    debugging

---
+++ source/problem/SWE/main_swe.cpp
@@ -0,0 +1,26 @@
+#include "../../general_definitions.hpp"
+
+#include "../../simulation/stepper.hpp"
+#include "swe_problem.hpp"
+#include "swe_kernels.hpp"
+#include "../../simulation/simulation.hpp"
+
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        std::cerr << "Usage\n"
+                  << "    /path/to/DG_HYPER_SWE input_file\n";
+        return 1;
+    } else {
+    
+    Simulation<SWE::Problem> simulation(std::string(argv[1]));
+    
+    auto t1 = std::chrono::high_resolution_clock::now();
+    simulation.Run();
+    auto t2 = std::chrono::high_resolution_clock::now();
+
+    std::cout << "Time Elapsed (in us): " << std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count()
+              << "\n";
+
+    return 0;
+    }
+}
