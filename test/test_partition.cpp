f8da0abe0e2beb95572f23e87bf4b69b3db8cd9e
Kazbek Kazhyken <kazbek@kepler.ices.utexas.edu>
2017-09-02 17:18:19 -0500

    cleanup

--- test/test_partition.cpp
+++ test/test_partition.cpp
@@ -16,13 +16,13 @@ bool check_partition(const MeshMetaData& mesh, std::vector<std::vector<MeshMetaD
     //  1.) Check that all the elements can be found in one of the submeshes
     {
         std::unordered_set<uint> elts;
-        for (auto& e : mesh._elements) {
+        for (auto& e : mesh.elements) {
             elts.insert(e.first);
         }
 
         for (auto& loc : submeshes) {
             for (auto& sm : loc) {
-                for (auto& e : sm._elements) {
+                for (auto& e : sm.elements) {
                     elts.erase(e.first);
                 }
             }
@@ -42,14 +42,13 @@ bool check_partition(const MeshMetaData& mesh, std::vector<std::vector<MeshMetaD
     //  2.) Check that all the edges between submeshes are set correctly
     for (auto& loc : submeshes) {
         for (auto& sm : loc) {
-            for (auto& e : sm._elements) {
+            for (auto& e : sm.elements) {
                 for (uint k = 0; k < e.second.neighbor_ID.size(); ++k) {
-                    if (e.second.boundary_type[k] == INTERNAL && !sm._elements.count(e.second.neighbor_ID[k])) {
+                    if (e.second.boundary_type[k] == INTERNAL && !sm.elements.count(e.second.neighbor_ID[k])) {
                         error_found = true;
                         std::cerr << "Element " << e.first << " cannot find internal neighbor of id "
                                   << e.second.neighbor_ID[k] << '\n';
-                    } else if (e.second.boundary_type[k] == DISTRIBUTED &&
-                               sm._elements.count(e.second.neighbor_ID[k])) {
+                    } else if (e.second.boundary_type[k] == DISTRIBUTED && sm.elements.count(e.second.neighbor_ID[k])) {
                         error_found = true;
                         std::cerr << "Element " << e.first << " found distributed neighbor on its own submeh\n";
                     }
