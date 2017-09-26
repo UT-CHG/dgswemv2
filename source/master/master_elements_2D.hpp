f8b995db17184cb547c05c4dc1dd06b8feb1420d
Maximilian <bremerm31@gmail.com>
2017-09-26 15:34:05 -0500

    Adding doxygen documentation for:  - source/utilities/ignore.hpp  - source/utilities/file_exists.hpp  - source/utilities/almost_equal.hpp  - source/master/master_elements_2D.hpp

--- source/master/master_elements_2D.hpp
+++ source/master/master_elements_2D.hpp
@@ -4,15 +4,47 @@
 #include "../general_definitions.hpp"
 
 namespace Master {
+/**
+ * Triangular master element.
+ * Triangular master elements contains the information necessary for evaluating computations on
+ * on the master triangle.
+ */
 template <class BasisType, class IntegrationType>
 class Triangle : public Master<2> {
   public:
+    /**
+     * The basis used over the elment.
+     */
     BasisType basis;
+
+    /**
+     * Integration rule used for evaluating integrals over the element.
+     */
     IntegrationType integration;
 
   public:
+    /**
+     * Construct a master triangle with polynomial order p.
+     *
+     * @param p Polynomial order
+     */
     Triangle(const uint p);
 
+    /**
+     * Transform coordinates on a boundary to master element coordinates.
+     * The function accepts a boundary ID and a vector of points located on that boundary
+     * and returns their coordinates on the master triangle. Based on boundary ID the endpoints
+     * are mapped to the following coordinates:
+     *
+     * Boundary ID | -1 Mapped To: | 1 Mapped To:
+     * ------------|---------------|-------------
+     *      0      |   ( 1,-1)     |   (-1, 1)
+     *      1      |   (-1, 1)     |   (-1,-1)
+     *      2      |   (-1,-1)     |   ( 1,-1)
+     *
+     * @param bound_id The ID of the boundary
+     * @param The points on the boundary
+     */
     std::vector<Point<2>> BoundaryToMasterCoordinates(const uint bound_id, const std::vector<Point<1>>& z_boundary);
 
   private:
--- source/utilities/file_exists.hpp
+++ source/utilities/file_exists.hpp
@@ -2,8 +2,15 @@
 #define FILE_EXISTS_HPP
 
 namespace Utilities {
-// This method of checking if a file exists was based on
-// https://stackoverflow.com/a/12774387
+/**
+ * Determine whether a file exists.
+ * Uses fopen to check if a file exists. This method of checking if a file exists was based on
+ * performance measurements done by
+ * <a href="https://stackoverflow.com/a/12774387">PherricOxide's answer</a>.
+ *
+ * @param file_name name of file
+ * @return Whether or not the file exists
+ */
 inline bool file_exists(const std::string& file_name) {
     if (FILE* file = fopen(file_name.c_str(), "r")) {
         fclose(file);
