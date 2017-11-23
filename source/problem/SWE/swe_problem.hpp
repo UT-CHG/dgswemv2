e63ba9399fb3d19b0e45f15d50ea9668f6ce65fe
kazbekkm <kazbek@ices.utexas.edu>
2017-11-23 01:41:20 -0600

    fixes

--- source/problem/SWE/swe_problem.hpp
+++ source/problem/SWE/swe_problem.hpp
@@ -77,12 +77,20 @@ struct Problem {
 
     template <typename ElementType>
     static void slope_limiting_prepare_element_kernel(const Stepper& stepper,
-                                                      ElementType& elt) template <typename InterfaceType>
+                                                      ElementType& elt);
+                                                      
+                                                       template <typename InterfaceType>
     static void slope_limiting_prepare_interface_kernel(const Stepper& stepper,
-                                                        InterfaceType& intface) template <typename BoundaryType>
+                                                        InterfaceType& intface);
+                                                        
+                                                         template <typename BoundaryType>
     static void slope_limiting_prepare_boundary_kernel(const Stepper& stepper,
-                                                       BoundaryType& bound) template <typename ElementType>
-    static void slope_limiting_kernel(const Stepper& stepper, ElementType& elt) template <typename ElementType>
+                                                       BoundaryType& bound);
+                                                       
+                                                        template <typename ElementType>
+    static void slope_limiting_kernel(const Stepper& stepper, ElementType& elt);
+    
+     template <typename ElementType>
     static void swap_states_kernel(const Stepper& stepper, ElementType& elt);
 
     // postprocessor kernels
