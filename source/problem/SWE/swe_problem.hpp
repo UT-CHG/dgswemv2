d3777aea89d117e13d649aa194b15b5cfbd0714d
kazbekkm <kazbek@ices.utexas.edu>
2017-11-23 01:54:57 -0600

    edits

--- source/problem/SWE/swe_problem.hpp
+++ source/problem/SWE/swe_problem.hpp
@@ -91,13 +91,7 @@ struct Problem {
     static void swap_states_kernel(const Stepper& stepper, ElementType& elt);
 
     // postprocessor kernels
-    template <typename ElementType>
-    static void extract_VTK_data_kernel(ElementType& elt, Array2D<double>& cell_data, Array2D<double>& point_data);
-
     static void write_VTK_data_kernel(ProblemMeshType& mesh, std::ofstream& raw_data_file);
-
-    template <typename ElementType>
-    static void extract_modal_data_kernel(ElementType& elt, std::vector<std::pair<uint, Array2D<double>>>& modal_data);
 
     static void write_modal_data_kernel(const Stepper& stepper, ProblemMeshType& mesh, const std::string& output_path);
 
