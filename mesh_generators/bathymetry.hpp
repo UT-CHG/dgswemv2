67a5c76da2e8553d8cfb34e57f233fa6c522ac7d
kazbekkm <kazbek@ices.utexas.edu>
2018-03-29 20:58:35 -0500

    bug radians to degrees

--- mesh_generators/bathymetry.hpp
+++ mesh_generators/bathymetry.hpp
@@ -3,7 +3,7 @@
 
 double bathymetry_function(double x, double y) {
     return 200.0;  // bathymetry for storm
-                 // return -1 + (6.0 / 90000) * x;
+                   // return -1 + (6.0 / 90000) * x;
 }
 
 #endif
