b33ebaf502f11c55cac4d700198dcfbd8b2cfa8d
Maximilian Bremer <max@ices.utexas.edu>
2018-06-06 14:03:31 -0500

    Adding Unit tests and one parallel correctness test
    - removing `scripts/circleci` (unnecessary)
    - updated parallel tests to optionally look for custom `DGSWEMV2_ROOT` variables

--- scripts/correctness/test_parallel_correctness.sh
+++ scripts/correctness/test_parallel_correctness.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-DGSWEMV2_ROOT="${HOME}/dgswemv2"
+if [ -z ${DGSWEMV2_ROOT+x} ]; then
+    DGSWEMV2_ROOT_="${HOME}/dgswemv2"
+else
+    DGSWEMV2_ROOT_=$DGSWEMV2_ROOT
+fi
 
 echo "Building mesh for manufactured solution..."
-cd $DGSWEMV2_ROOT/mesh_generators
+cd $DGSWEMV2_ROOT_/mesh_generators
 cat > bathymetry.hpp <<EOL
 #ifndef BATHYMETRY_HPP
 #define BATHYMETRY_HPP
@@ -15,7 +19,7 @@ double bathymetry_function(double x, double y) {
 EOL
 echo ""
 echo "Compiling code (if necessary)..."
-cd $DGSWEMV2_ROOT/build
+cd $DGSWEMV2_ROOT_/build
 make rectangular_mesh_generator
 make partitioner
 make MANUFACTURED_SOLUTION_SERIAL
@@ -25,29 +29,29 @@ echo ""
 echo "Setting up runtime files..."
 cd $HOME
 mkdir -p dgswemv2_test
-cp -r $DGSWEMV2_ROOT/examples/manufactured_solution/input_files/* dgswemv2_test
+cp -r $DGSWEMV2_ROOT_/examples/manufactured_solution/input_files/* dgswemv2_test
 
 cd dgswemv2_test
 #Halve the manufactured solution run time to shorten circleci test time
 sed -i 's/  end_time: 3600                #in seconds/  end_time: 1800/g' dgswemv2_input.15
-$DGSWEMV2_ROOT/build/mesh_generators/rectangular_mesh_generator mesh_generator_input.yml
+$DGSWEMV2_ROOT_/build/mesh_generators/rectangular_mesh_generator mesh_generator_input.yml
 
 echo "Running Serial Test case..."
 rm -f serial.out
-$DGSWEMV2_ROOT/build/examples/MANUFACTURED_SOLUTION_SERIAL dgswemv2_input.15 &> serial.out
+$DGSWEMV2_ROOT_/build/examples/MANUFACTURED_SOLUTION_SERIAL dgswemv2_input.15 &> serial.out
 
 echo "Running HPX Test case..."
 rm -f hpx.out
-$DGSWEMV2_ROOT/build/partitioner/partitioner dgswemv2_input.15 2 1
-$DGSWEMV2_ROOT/build/examples/MANUFACTURED_SOLUTION_HPX dgswemv2_input_parallelized.15 --hpx:threads=2 &> hpx.out
+$DGSWEMV2_ROOT_/build/partitioner/partitioner dgswemv2_input.15 2 1
+$DGSWEMV2_ROOT_/build/examples/MANUFACTURED_SOLUTION_HPX dgswemv2_input_parallelized.15 --hpx:threads=2 &> hpx.out
 
 echo "Running MPI Test case..."
 rm rectangular_mesh_*
 rm -f ompi.out
-$DGSWEMV2_ROOT/build/partitioner/partitioner dgswemv2_input.15 2 1 2
+$DGSWEMV2_ROOT_/build/partitioner/partitioner dgswemv2_input.15 2 1 2
 #Since OpenMPI does not by default support MPI_THREAD_MULTIPLE
 # we set OMP_NUM_THREADS=1
-OMP_NUM_THREADS=1 mpirun -np 2 $DGSWEMV2_ROOT/build/examples/MANUFACTURED_SOLUTION_OMPI dgswemv2_input_parallelized.15 &> ompi.out
+OMP_NUM_THREADS=1 mpirun -np 2 $DGSWEMV2_ROOT_/build/examples/MANUFACTURED_SOLUTION_OMPI dgswemv2_input_parallelized.15 &> ompi.out
 
-python $DGSWEMV2_ROOT/scripts/correctness/compare_l2_errors.py
+python $DGSWEMV2_ROOT_/scripts/correctness/compare_l2_errors.py
 exit $?
--- scripts/correctness/test_parallel_weirs.sh
+++ scripts/correctness/test_parallel_weirs.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
-DGSWEMV2_ROOT="${HOME}/dgswemv2"
+if [ -z ${DGSWEMV2_ROOT+x} ]; then
+    DGSWEMV2_ROOT_="${HOME}/dgswemv2"
+else
+    DGSWEMV2_ROOT_=$DGSWEMV2_ROOT
+fi
 DGSWEMV2_TEST="${HOME}/dgswemv2_parallel_weirs_test"
 
 echo "Running test to check that distributed weirs are correctly parallelized"
 echo "Compiling code (if necessary)..."
-cd $DGSWEMV2_ROOT/build
+cd $DGSWEMV2_ROOT_/build
 make partitioner
 #make DG_HYPER_SWE_HPX
 #make DG_HYPER_SWE_OMPI
 echo ""
 echo "Setting up runtime files..."
 mkdir -p ${DGSWEMV2_TEST}
-cp -r $DGSWEMV2_ROOT/test/files_for_testing/weir/* ${DGSWEMV2_TEST}
+cp -r $DGSWEMV2_ROOT_/test/files_for_testing/weir/* ${DGSWEMV2_TEST}
 
 cd ${DGSWEMV2_TEST}
 #Halve the manufactured solution run time to shorten circleci test time
@@ -20,11 +24,11 @@ cd ${DGSWEMV2_TEST}
 
 echo ""
 echo "seting up serial test case"
-MAIN_DIR="${DGSWEMV2_ROOT}/source/problem/SWE/main_files"
+MAIN_DIR="${DGSWEMV2_ROOT_}/source/problem/SWE/main_files"
 sed -i.tmp '/return 0/i\
         simulation.ComputeL2Residual();\
 ' ${MAIN_DIR}/main_swe.cpp
-cd ${DGSWEMV2_ROOT}/build
+cd ${DGSWEMV2_ROOT_}/build
 make DG_HYPER_SWE_SERIAL
 cd $MAIN_DIR
 #undo the addition of the ComputeL2Residual call
@@ -33,18 +37,18 @@ echo ""
 echo "Running Serial Test case..."
 cd $DGSWEMV2_TEST
 rm -f serial.out
-$DGSWEMV2_ROOT/build/source/DG_HYPER_SWE_SERIAL dgswemv2_input.15 &> serial.out
+$DGSWEMV2_ROOT_/build/source/DG_HYPER_SWE_SERIAL dgswemv2_input.15 &> serial.out
 
 echo ""
 echo "Building HPX Test case..."
 cd $DGSWEMV2_TEST
 rm weir_*
-$DGSWEMV2_ROOT/build/partitioner/partitioner dgswemv2_input.15 3 1
+$DGSWEMV2_ROOT_/build/partitioner/partitioner dgswemv2_input.15 3 1
 sed -i.tmp '/    return hpx::finalize();/i\
     hpx::future<double> globalResidualL2 = ComputeL2Residual(simulation_clients);\
     std::cout << "L2 error: " << std::setprecision(14) << std::sqrt(globalResidualL2.get()) << std::endl;\
 ' ${MAIN_DIR}/hpx_main_swe.cpp
-cd ${DGSWEMV2_ROOT}/build
+cd ${DGSWEMV2_ROOT_}/build
 make DG_HYPER_SWE_HPX
 cd $MAIN_DIR
 mv hpx_main_swe.cpp.tmp hpx_main_swe.cpp
@@ -52,14 +56,14 @@ echo ""
 echo "Running HPX Test case..."
 cd $DGSWEMV2_TEST
 rm -f hpx.out
-$DGSWEMV2_ROOT/build/source/DG_HYPER_SWE_HPX dgswemv2_input_parallelized.15 --hpx:threads=3 &> hpx.out
+$DGSWEMV2_ROOT_/build/source/DG_HYPER_SWE_HPX dgswemv2_input_parallelized.15 --hpx:threads=3 &> hpx.out
 
 echo ""
 echo "Building MPI Test case..."
 sed -i.tmp '/        MPI_Finalize();/i\
         simulation.ComputeL2Residual();\
 ' ${MAIN_DIR}/ompi_main_swe.cpp
-cd ${DGSWEMV2_ROOT}/build
+cd ${DGSWEMV2_ROOT_}/build
 make DG_HYPER_SWE_OMPI
 cd $MAIN_DIR
 mv ompi_main_swe.cpp.tmp ompi_main_swe.cpp
@@ -70,11 +74,11 @@ echo ""
 echo "Running OMPI Test case..."
 cd $DGSWEMV2_TEST
 rm weir_*
-$DGSWEMV2_ROOT/build/partitioner/partitioner dgswemv2_input.15 3 1 3
+$DGSWEMV2_ROOT_/build/partitioner/partitioner dgswemv2_input.15 3 1 3
 rm -f ompi.out
 #Since OpenMPI does not by default support MPI_THREAD_MULTIPLE
 # we set OMP_NUM_THREADS=1
-OMP_NUM_THREADS=1 mpirun -np 3 $DGSWEMV2_ROOT/build/source/DG_HYPER_SWE_OMPI dgswemv2_input_parallelized.15 &> ompi.out
+OMP_NUM_THREADS=1 mpirun -np 3 $DGSWEMV2_ROOT_/build/source/DG_HYPER_SWE_OMPI dgswemv2_input_parallelized.15 &> ompi.out
 
-python $DGSWEMV2_ROOT/scripts/correctness/compare_l2_errors.py
+python $DGSWEMV2_ROOT_/scripts/correctness/compare_l2_errors.py
 exit $?
