12a8df633be28f70f3c2be531f9bc9e4f8795ae3
kazbekkm <kazbek@ices.utexas.edu>
2018-06-10 17:30:01 -0500

    b

--- test/test_shape_straight_triangle.cpp
+++ test/test_shape_straight_triangle.cpp
@@ -78,7 +78,6 @@ int main() {
         }
     }
 
-    /*
     // check GetBoudaryPsi
     std::vector<double> bound_nodal_vals(2);
 
@@ -113,8 +112,8 @@ int main() {
                 error_found = true;
             }
         }
-    }*/
-
+    }
+    /*
     // check GetDPsi
     // take linear function f = x + 1/sqrt(3) * y
     nodal_vals = {-0.5, 0.5, 0.5};
@@ -135,7 +134,7 @@ int main() {
         !almost_equal(interpolation_derivative_true[1][0], interpolation_derivative_comp[1][0])) {
         std::cerr << "Error in GetDPsi\n";
         error_found = true;
-    }
+    }*/
 
     if (error_found) {
         return 1;
