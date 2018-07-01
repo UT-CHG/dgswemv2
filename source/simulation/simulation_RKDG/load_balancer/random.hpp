04bc1d7c7ef891910c88fb0c2e09354235944b59
bremerm31 <bremerm31@gmail.com>
2018-07-01 13:23:43 -0500

    Turning off load balancer till #89 gets fixed

--- source/simulation/simulation_RKDG/load_balancer/random.hpp
+++ source/simulation/simulation_RKDG/load_balancer/random.hpp
@@ -127,7 +127,8 @@ WorldModel<ProblemType>::WorldModel(const std::string& input_string) {
 
 template <typename ProblemType>
 void WorldModel<ProblemType>::MigrateOneSubmesh() {
-    if ( !this->tried_moving_one_tile ) {
+    if ( false ) {
+//    if ( !this->tried_moving_one_tile ) {
         const std::vector<hpx::naming::id_type> localities = hpx::find_all_localities();
 
         client_locality_id_pair& curr_target = this->simulation_unit_clients[ 0xdeadbeef % this->simulation_unit_clients.size() ];
