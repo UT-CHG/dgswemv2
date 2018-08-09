645effcd0f3e054f0b6d5a1a68c258eff1754d06
Maximilian Bremer <bremerm31@gmail.com>
2018-08-09 09:40:54 -0500

    Formatting

--- test/test_eigen_serialization.cpp
+++ test/test_eigen_serialization.cpp
@@ -18,8 +18,7 @@ bool test_serialize_eigen(Matrix& i_mat) {
 
     Matrix residual = i_mat - o_mat;
 
-    std::cout << "In object:\n" << i_mat << '\n'
-              << "Out object:\n" << o_mat << '\n';
+    std::cout << "In object:\n" << i_mat << '\n' << "Out object:\n" << o_mat << '\n';
 
     error_found |= !Utilities::almost_equal(0., residual.norm());
 
@@ -45,14 +44,30 @@ int main() {
         a << 1, 2, 3, 4, 5, 6, 8, 9, 10, 11, 12, 7;
         error_found |= test_serialize_eigen(a);
 
-        DynMatrix<double> b = DynMatrix<double>::Zero(4,4);
-        b(0,0) = 1; b(0,1) = 2; b(0,2) = 3; b(0,3) = 4;
-        b(2,0) = 1; b(2,1) = 2;
-        b(3,0) = 1; b(3,1) = 2; b(3,2) = 3; b(3,3) = 5;
+        DynMatrix<double> b = DynMatrix<double>::Zero(4, 4);
+        b(0, 0)             = 1;
+        b(0, 1)             = 2;
+        b(0, 2)             = 3;
+        b(0, 3)             = 4;
+        b(2, 0)             = 1;
+        b(2, 1)             = 2;
+        b(3, 0)             = 1;
+        b(3, 1)             = 2;
+        b(3, 2)             = 3;
+        b(3, 3)             = 5;
         error_found |= test_serialize_eigen(b);
 
-        SparseMatrix<double> d(4,4);
-        std::vector<Eigen::Triplet<double>> coefficients{{0,0,1},{0,1,2},{0,2,3},{0,3,4},{2,0,1},{2,2,2},{3,0,1},{3,1,2},{3,2,3},{3,3,5}};
+        SparseMatrix<double> d(4, 4);
+        std::vector<Eigen::Triplet<double>> coefficients{{0, 0, 1},
+                                                         {0, 1, 2},
+                                                         {0, 2, 3},
+                                                         {0, 3, 4},
+                                                         {2, 0, 1},
+                                                         {2, 2, 2},
+                                                         {3, 0, 1},
+                                                         {3, 1, 2},
+                                                         {3, 2, 3},
+                                                         {3, 3, 5}};
         d.setFromTriplets(coefficients.begin(), coefficients.end());
         error_found |= test_serialize_eigen(d);
     }
