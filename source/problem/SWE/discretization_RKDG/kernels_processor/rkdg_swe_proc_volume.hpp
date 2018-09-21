2407aa98c8560864f61851cbe87438b82156acdb
kazbekkm <kazbek@ices.utexas.edu>
2018-09-21 00:02:51 -0500

    Update rkdg_swe_proc_volume.hpp

--- source/problem/SWE/discretization_RKDG/kernels_processor/rkdg_swe_proc_volume.hpp
+++ source/problem/SWE/discretization_RKDG/kernels_processor/rkdg_swe_proc_volume.hpp
@@ -6,10 +6,12 @@ namespace RKDG {
 template <typename ElementType>
 void Problem::volume_kernel(const RKStepper& stepper, ElementType& elt) {
     const uint stage = stepper.GetStage();
+
     auto& wd_state   = elt.data.wet_dry_state;
     auto& state      = elt.data.state[stage];
 
     set_constant(state.rhs, 0.0);
+    
     if (wd_state.wet) {
         auto& internal = elt.data.internal;
 
