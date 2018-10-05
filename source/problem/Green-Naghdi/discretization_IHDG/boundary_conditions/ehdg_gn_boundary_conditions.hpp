0e88e1fd9279286706e1d5c3fd017e74d189c79f
kazbekkm <kazbek@ices.utexas.edu>
2018-10-04 20:00:20 -0500

    remove GN IHDG

--- source/problem/Green-Naghdi/discretization_IHDG/boundary_conditions/ehdg_gn_boundary_conditions.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef IHDG_GN_BOUNDARY_CONDITIONS_HPP
-#define IHDG_GN_BOUNDARY_CONDITIONS_HPP
-
-#include "ihdg_gn_bc_land.hpp"
-#include "ihdg_gn_bc_flow.hpp"
-#include "ihdg_gn_bc_tide.hpp"
-
-#endif
--- source/problem/Green-Naghdi/discretization_IHDG/stabilization_parameters/ihdg_gn_stabilization_params.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef IHDG_GN_STABILIZATION_PARAMS_HPP
-#define IHDG_GN_STABILIZATION_PARAMS_HPP
-
-#include "ihdg_gn_stab_param_LF.hpp"
-
-#endif
