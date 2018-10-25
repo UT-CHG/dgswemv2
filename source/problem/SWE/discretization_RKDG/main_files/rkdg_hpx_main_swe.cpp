c63928de0a7d1b306431b668cf0cf6137d13673e
kazbekkm <kazbek@ices.utexas.edu>
2018-10-25 10:01:30 -0500

    fix?

--- source/problem/SWE/discretization_RKDG/main_files/rkdg_hpx_main_swe.cpp
+++ source/problem/SWE/discretization_RKDG/main_files/rkdg_hpx_main_swe.cpp
@@ -14,7 +14,6 @@
 
 #include "problem/SWE/discretization_RKDG/rkdg_swe_problem.hpp"
 #include "problem/SWE/discretization_RKDG/kernels_preprocessor/rkdg_swe_kernels_preprocessor.hpp"
-#include "problem/SWE/discretization_RKDG/kernels_postprocessor/rkdg_swe_kernels_postprocessor.hpp"
 
 #include "problem/SWE/discretization_RKDG/kernels_preprocessor/rkdg_swe_pre_hpx.hpp"
 #include "problem/SWE/discretization_RKDG/kernels_processor/rkdg_swe_proc_hpx_stage.hpp"
