6ca68dad718052e30f3aa61b5412a2f250e1a47c
Maximilian Bremer <max@ices.utexas.edu>
2018-12-09 13:15:45 -0600

    Updating test_llf_flux to test all fluxes simultaneously

--- test/test_llf_flux.cpp
+++ test/test_llf_flux.cpp
@@ -21,97 +21,91 @@ bool test_configuration(const int configuration,
                         const double true_qy_flux) {
     bool error_found = false;
 
-    { // pass using doubles
-        std::array<double,3> aux_in{bath, ze_in + bath, sp};
-        std::array<double,2> norm{normal[0], normal[1]};
+    // pass using doubles
+    std::array<double,3> aux_in{bath, ze_in + bath, sp};
+    std::array<double,2> norm{normal[0], normal[1]};
 
-        std::array<double,3> F_hat;
-        SWE::RKDG::LLF_flux( SWE::Global::g,
-                             ze_in,
-                             qx_in,
-                             qy_in,
-                             ze_ex,
-                             qx_ex,
-                             qy_ex,
-                             aux_in,
-                             norm,
-                             F_hat);
+    std::array<double,3> F_hat;
+    SWE::RKDG::LLF_flux( SWE::Global::g,
+                         ze_in,
+                         qx_in,
+                         qy_in,
+                         ze_ex,
+                         qx_ex,
+                         qy_ex,
+                         aux_in,
+                         norm,
+                         F_hat);
 
-        if (!Utilities::almost_equal(F_hat[0], true_ze_flux)) {
-            std::cerr << "Error in configuration " << configuration << " in surface elevation flux\n";
-            std::cerr << "Got: " << F_hat[0] << " Should be:  " << true_ze_flux << "\n";
-            error_found = true;
-        }
-
-        if (!Utilities::almost_equal(F_hat[1], true_qx_flux)) {
-            std::cerr << "Error in configuration " << configuration << " in x-momentum flux\n";
-            std::cerr << "Got: " << F_hat[1] << " Should be:  " << true_qx_flux << "\n";
-            error_found = true;
-        }
-
-        if (!Utilities::almost_equal(F_hat[2], true_qy_flux)) {
-            std::cerr << "Error in configuration 1 in y-momentum flux\n";
-            std::cerr << "Got: " << F_hat[2] << " Should be:  " << true_qy_flux << "\n";
-            error_found = true;
-        }
+    if (!Utilities::almost_equal(F_hat[0], true_ze_flux)) {
+        std::cerr << "Error in configuration " << configuration << " in surface elevation flux\n";
+        std::cerr << "Got: " << F_hat[0] << " Should be:  " << true_ze_flux << "\n";
+        error_found = true;
     }
 
-    { //pass using input array
-        DynVector<double> ze_in_(1), qx_in_(1), qy_in_(1), ze_ex_(1), qx_ex_(1), qy_ex_(1);
-        ze_in_[0] = ze_in;
-        qx_in_[0] = qx_in;
-        qy_in_[0] = qy_in;
-        ze_ex_[0] = ze_ex;
-        qx_ex_[0] = qx_ex;
-        qy_ex_[0] = qy_ex;
+    if (!Utilities::almost_equal(F_hat[1], true_qx_flux)) {
+        std::cerr << "Error in configuration " << configuration << " in x-momentum flux\n";
+        std::cerr << "Got: " << F_hat[1] << " Should be:  " << true_qx_flux << "\n";
+        error_found = true;
+    }
 
-        std::array<DynVector<double>,3> aux_in;
-        aux_in.fill(DynVector<double>(1));
-        aux_in[0][0] = bath;
-        aux_in[1][0] = ze_in + bath;
-        aux_in[2][0] = sp;
-
-        std::array<DynVector<double>,2> norm;
-        norm.fill(DynVector<double>(1));
-        norm[0][0] = normal[0];
-        norm[1][0] = normal[1];
-
-        std::array<DynVector<double>,3> F_hat;
-        F_hat.fill(DynVector<double>(1));
-
-        SWE::RKDG::LLF_flux( SWE::Global::g,
-                             ze_in_,
-                             qx_in_,
-                             qy_in_,
-                             ze_ex_,
-                             qx_ex_,
-                             qy_ex_,
-                             aux_in,
-                             norm,
-                             F_hat);
-
-        if (!Utilities::almost_equal(F_hat[0][0], true_ze_flux)) {
-            std::cerr << "Error in configuration " << configuration << " in surface elevation flux\n";
-            std::cerr << "Got: " << F_hat[0][0] << " Should be:  " << true_ze_flux << "\n";
-            error_found = true;
-        }
-
-        if (!Utilities::almost_equal(F_hat[1][0], true_qx_flux)) {
-            std::cerr << "Error in configuration " << configuration << " in x-momentum flux\n";
-            std::cerr << "Got: " << F_hat[1][0] << " Should be:  " << true_qx_flux << "\n";
-            error_found = true;
-        }
-
-        if (!Utilities::almost_equal(F_hat[2][0], true_qy_flux)) {
-            std::cerr << "Error in configuration 1 in y-momentum flux\n";
-            std::cerr << "Got: " << F_hat[2][0] << " Should be:  " << true_qy_flux << "\n";
-            error_found = true;
-        }
-
+    if (!Utilities::almost_equal(F_hat[2], true_qy_flux)) {
+        std::cerr << "Error in configuration " << configuration << " in y-momentum flux\n";
+        std::cerr << "Got: " << F_hat[2] << " Should be:  " << true_qy_flux << "\n";
+        error_found = true;
     }
 
     return error_found;
 }
+
+bool test_configuration_vectorized(const DynRowVector<double>& ze_in,
+                                   const DynRowVector<double>& ze_ex,
+                                   const DynRowVector<double>& qx_in,
+                                   const DynRowVector<double>& qx_ex,
+                                   const DynRowVector<double>& qy_in,
+                                   const DynRowVector<double>& qy_ex,
+                                   const std::array<DynRowVector<double>, SWE::n_auxiliaries>& aux,
+                                   const std::array<DynRowVector<double>, SWE::n_dimensions>& surface_normal,
+                                   const DynRowVector<double>& true_ze_flux,
+                                   const DynRowVector<double>& true_qx_flux,
+                                   const DynRowVector<double>& true_qy_flux) {
+    bool error_found = false;
+
+    std::array<DynRowVector<double>,3> F_hat;
+    F_hat.fill(DynRowVector<double>(ze_in.size()));
+    SWE::RKDG::LLF_flux( SWE::Global::g,
+                         ze_in,
+                         qx_in,
+                         qy_in,
+                         ze_ex,
+                         qx_ex,
+                         qy_ex,
+                         aux,
+                         surface_normal,
+                         F_hat);
+
+    double ze_norm = norm( F_hat[SWE::Variables::ze] - true_ze_flux);
+    if (!Utilities::almost_equal(0,ze_norm)) {
+        std::cerr << "Error in vectorized configuration in surface elevation flux\n";
+        error_found = true;
+    }
+
+    double qx_norm = norm( F_hat[SWE::Variables::qx] - true_qx_flux);
+    if (!Utilities::almost_equal(0, qx_norm)) {
+        std::cerr << "Error in vectorized configuration in x-momentum flux\n";
+        error_found = true;
+    }
+
+    double qy_norm = norm( F_hat[SWE::Variables::qy] - true_qy_flux);
+    if (!Utilities::almost_equal(0,qy_norm)) {
+        std::cerr << "Error in vectorized_configuration in y-momentum flux\n";
+        error_found = true;
+    }
+
+    return error_found;
+
+}
+
 
 int main() {
     using Utilities::almost_equal;
@@ -123,95 +117,97 @@ int main() {
 
     bool error_found = false;
 
+    DynRowVector<double> ze_in(3), ze_ex(3), qx_in(3), qx_ex(3), qy_in(3), qy_ex(3);
+
+    ze_in[0] = 1.0;
+    ze_in[1] = 1.0;
+    ze_in[2] = 1.0;
+
+    ze_ex[0] = 1.5;
+    ze_ex[1] = 1.5;
+    ze_ex[2] = 1.5;
+
+    qx_in[0] = 2.2;
+    qx_in[1] = 0;
+    qx_in[2] = 5.2;
+
+    qx_ex[0] = 3.2;
+    qx_ex[1] = 0;
+    qx_ex[2] = 6.2;
+
+    qy_in[0] = 0.1;
+    qy_in[1] = 0;
+    qy_in[2] = 7.1;
+
+    qy_ex[0] = -0.2;
+    qy_ex[1] = 0;
+    qy_ex[2] = 6.2;
+
+
+    std::array<DynRowVector<double>, SWE::n_auxiliaries> aux;
+    aux.fill(DynRowVector<double>(3));
+    set_constant(aux[SWE::Auxiliaries::bath], 0);
+    set_constant(aux[SWE::Auxiliaries::sp], 1);
+
+    std::array<DynRowVector<double>, SWE::n_dimensions> surface_normal;
+    surface_normal.fill(DynRowVector<double>(3));
+    surface_normal[GlobalCoord::x][0] = 1. / std::sqrt(2.);
+    surface_normal[GlobalCoord::x][1] = 1. / std::sqrt(2.);
+    surface_normal[GlobalCoord::x][2] = 1. / std::sqrt(2.);
+
+    surface_normal[GlobalCoord::y][0] = 1. / std::sqrt(2.);
+    surface_normal[GlobalCoord::y][1] = -1. / std::sqrt(2.);
+    surface_normal[GlobalCoord::y][2] = 1. / std::sqrt(2.);
+
+    DynRowVector<double> true_ze_flux(3), true_qx_flux(3), true_qy_flux(3);
+    true_ze_flux[0] = 0.561276190610246;
+    true_qx_flux[0] = 7.062691284925730;
+    true_qy_flux[0] = 6.363512979114632;
+
+    true_ze_flux[1] = -0.959003388940832;
+    true_qx_flux[1] = 5.636082987795024;
+    true_qy_flux[1] = -5.636082987795024;
+
+    true_ze_flux[2] = 5.775392407336936;
+    true_qx_flux[2] = 40.455394948714215;
+    true_qy_flux[2] = 59.955967780083341;
+
     // we test a few configurations
-    {  // Configuration 1
-        double ze_in = 1.0;
-        double ze_ex = 1.5;
-        double qx_in = 2.2;
-        double qx_ex = 3.2;
-        double qy_in = 0.1;
-        double qy_ex = -0.2;
+    for (uint config_id = 0; config_id < 3; ++config_id) {  // Configuration 1
 
-        StatVector<double, 2> normal{1. / std::sqrt(2.), 1. / std::sqrt(2.)};
+        StatVector<double, 2> normal{surface_normal[GlobalCoord::x][config_id],
+                surface_normal[GlobalCoord::y][config_id]};
 
-        double bath = 0;
-        double sp   = 1;
-
-        if (test_configuration(1,
-                               ze_in,
-                               ze_ex,
-                               qx_in,
-                               qx_ex,
-                               qy_in,
-                               qy_ex,
-                               bath,
-                               sp,
+        if (test_configuration(config_id,
+                               ze_in[config_id],
+                               ze_ex[config_id],
+                               qx_in[config_id],
+                               qx_ex[config_id],
+                               qy_in[config_id],
+                               qy_ex[config_id],
+                               aux[SWE::Auxiliaries::bath][config_id],
+                               aux[SWE::Auxiliaries::sp][config_id],
                                normal,
-                               0.561276190610246,
-                               7.062691284925730,
-                               6.363512979114632)) {
+                               true_ze_flux[config_id],
+                               true_qx_flux[config_id],
+                               true_qy_flux[config_id])) {
             error_found = true;
         }
     }
 
-    {  // Configuration 2
-        double ze_in = 1.0;
-        double ze_ex = 1.5;
-        double qx_in = 0;
-        double qx_ex = 0;
-        double qy_in = 0;
-        double qy_ex = 0;
-
-        StatVector<double, 2> normal{1. / std::sqrt(2.), -1. / std::sqrt(2.)};
-
-        double bath = 0;
-        double sp   = 1;
-
-        if (test_configuration(2,
-                               ze_in,
-                               ze_ex,
-                               qx_in,
-                               qx_ex,
-                               qy_in,
-                               qy_ex,
-                               bath,
-                               sp,
-                               normal,
-                               -0.959003388940832,
-                               5.636082987795024,
-                               -5.636082987795024)) {
-            error_found = true;
-        }
-    }
-
-    {  // Configuration 3
-        double ze_in = 1.0;
-        double ze_ex = 1.5;
-        double qx_in = 5.2;
-        double qx_ex = 6.2;
-        double qy_in = 7.1;
-        double qy_ex = 6.2;
-
-        StatVector<double, 2> normal{1. / std::sqrt(2.), 1. / std::sqrt(2.)};
-
-        double bath = 0;
-        double sp   = 1;
-
-        if (test_configuration(2,
-                               ze_in,
-                               ze_ex,
-                               qx_in,
-                               qx_ex,
-                               qy_in,
-                               qy_ex,
-                               bath,
-                               sp,
-                               normal,
-                               5.775392407336936,
-                               40.455394948714215,
-                               59.955967780083341)) {
-            error_found = true;
-        }
+    // Test all configurations at once
+    if ( test_configuration_vectorized(ze_in,
+                                       ze_ex,
+                                       qx_in,
+                                       qx_ex,
+                                       qy_in,
+                                       qy_ex,
+                                       aux,
+                                       surface_normal,
+                                       true_ze_flux,
+                                       true_qx_flux,
+                                       true_qy_flux)) {
+        error_found  = true;
     }
 
     if (error_found) {
