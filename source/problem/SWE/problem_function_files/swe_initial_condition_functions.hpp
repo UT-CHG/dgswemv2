3251f9ad46578a2f87ab5995e056665a81cbdd9e
kazbekkm <kazbek@ices.utexas.edu>
2019-09-26 17:49:12 -0500

    Accidental changes

--- source/problem/SWE/problem_function_files/swe_initial_condition_functions.hpp
+++ source/problem/SWE/problem_function_files/swe_initial_condition_functions.hpp
@@ -5,19 +5,8 @@
 
 namespace SWE {
 inline StatVector<double, SWE::n_variables> ic_q(const double t, const Point<2>& pt) {
-    constexpr double g = 9.81;
-
-    constexpr double ao = 0.05;
-    constexpr double Ho = 0.5;
-    constexpr double xo = -5.0;
-
-    constexpr double co = std::sqrt(g * (Ho + ao));
-    constexpr double w  = std::sqrt(3.0 * ao) / (2.0 * Ho * std::sqrt(Ho + ao));
-
-    double ic_ze = ao * std::pow(1.0 / std::cosh(w * (pt[GlobalCoord::x] - xo - co * t)), 2);
-
-    double ic_qx = co * ao * std::pow(1.0 / std::cosh(w * (pt[GlobalCoord::x] - xo - co * t)), 2);
-
+    double ic_ze = 0.0;
+    double ic_qx = 0.0;
     double ic_qy = 0.0;
 
     StatVector<double, SWE::n_variables> ic_q{ic_ze, ic_qx, ic_qy};
