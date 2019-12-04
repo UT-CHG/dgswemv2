84f2911ac9488c34dcc187b998505ec914e5038e
kazbekkm <kazbek@ices.utexas.edu>
2019-12-03 20:15:37 -0600

    bug

--- source/problem/SWE/problem_data_structure/swe_data_internal.hpp
+++ source/problem/SWE/problem_data_structure/swe_data_internal.hpp
@@ -54,7 +54,7 @@ struct Internal {
             & Fx_at_gp
             & Fy_at_gp
             & source_at_gp
-            & dbath_at_gp
+            & db_at_gp
             & tau_s_at_gp
             & dp_atm_at_gp
             & dtide_pot_at_gp;
