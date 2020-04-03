8ee275faa2d75a24848c1384777aa0c32ce7cce9
Maximilian Bremer <bremerm31@gmail.com>
2020-04-03 16:05:36 -0500

    Explicitly setting ends of Levees as land bdry
    There was a previous bug, where if the end of a levee was inside the
    domain it would not be assigned a boundary type causing the code to
    throw an error.

--- format_code.sh
+++ format_code.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 if [ "$#" -eq "0" ]; then
-    cmd="clang-format -i -style=file */*.cpp */*.hpp */*.tpp */*/*.cpp */*/*.hpp */*/*.tpp */*/*/*.cpp */*/*/*.hpp */*/*/*.tpp */*/*/*/*.cpp */*/*/*/*.hpp */*/*/*/*.tpp */*/*/*/*/*.cpp */*/*/*/*/*.hpp */*/*/*/*/*.tpp */*/*/*/*/*/*.cpp */*/*/*/*/*/*.hpp */*/*/*/*/*/*.tpp"
+    cmd="/home/bremerm31/clang+llvm-6.0.1-x86_64-linux-gnu-ubuntu-16.04/bin/clang-format -i -style=file */*.cpp */*.hpp */*.tpp */*/*.cpp */*/*.hpp */*/*.tpp */*/*/*.cpp */*/*/*.hpp */*/*/*.tpp */*/*/*/*.cpp */*/*/*/*.hpp */*/*/*/*.tpp */*/*/*/*/*.cpp */*/*/*/*/*.hpp */*/*/*/*/*.tpp */*/*/*/*/*/*.cpp */*/*/*/*/*/*.hpp */*/*/*/*/*/*.tpp"
     echo ${cmd}
     ${cmd}
 else
     for var in "$@"
     do
-	cmd="clang-format -i -style=file ${var}"
+	cmd="clang-format-5.0 -i -style=file ${var}"
 	echo ${cmd}
 	${cmd}
     done
